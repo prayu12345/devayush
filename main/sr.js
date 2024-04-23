1d96ebaa8148b1fd563f59c155f23f9411d5bce0
prayu12345 <164336616+prayu12345@users.noreply.github.com>
2024-04-23 23:39:04 +0530

    added index.html

---
+++ main/sr.js
@@ -0,0 +1,29 @@
+// Function to send Holi wishes
+function sendWishes() {
+    window.open("file:///C:/Users/akhil/OneDrive/Desktop/main/letter.html"); // Display a customized Holi wish message
+}
+
+// Function to play color splash animation
+function playlovenight() {
+    // You can implement an animation to simulate color splashes on the webpage
+    // For example, change background color, animate elements, etc.
+    window.open("file:///C:/Users/akhil/OneDrive/Desktop/main/Neon%20Heart/ayush2.html"); 
+}
+
+// Function to open Holi-themed playlist in a new tab or window
+function openwishlist() {
+    window.open("file:///C:/Users/akhil/OneDrive/Desktop/main/wishlist.html");
+}
+
+// Function to share Holi wishes on social media
+function wow() {
+    window.open("file:///C:/Users/akhil/OneDrive/Desktop/main/wow.html"); // Implement logic to share the Holi wishes on social media platforms
+}
+    // JavaScript function to change background color
+function changeBackgroundColor() {
+    var randomColor = '#' + Math.floor(Math.random()*16777215).toString(16); // Generate random hex color
+    document.body.style.backgroundColor = randomColor; // Apply the color to the body background
+}
+function special() {
+    window.open("file:///C:/Users/akhil/OneDrive/Desktop/photossss.html"); 
+}
